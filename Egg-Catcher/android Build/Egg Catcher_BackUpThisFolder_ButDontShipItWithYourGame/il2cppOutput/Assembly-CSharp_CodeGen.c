0ccbaa9e21ed4420a8372c7ed6168d15421598db
jamesfothergill <v8255920@live.tees.ac.uk>
2024-05-02 11:06:40 +0100

    Egg Catcher Game Completed
    Game files and android .APK

---
+++ Egg-Catcher/android Build/Egg Catcher_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,133 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BasketController::Start()
+extern void BasketController_Start_mC696348DB8DD127709ED40F9D590DF298DD8A8A2 (void);
+// 0x00000002 System.Void BasketController::Update()
+extern void BasketController_Update_m9C2E66B77A67B820A46A47AF6CAE2B1988A121B0 (void);
+// 0x00000003 System.Void BasketController::.ctor()
+extern void BasketController__ctor_m261695708574EA9EAA5F70C368F7E5F8966035B2 (void);
+// 0x00000004 System.Void Egg::Start()
+extern void Egg_Start_m2A6F76BB43BFB3A4F6E9272801C4102838A419AC (void);
+// 0x00000005 System.Void Egg::Update()
+extern void Egg_Update_m0B8158A925C8E1789B6E3890C76E375D86A62026 (void);
+// 0x00000006 System.Void Egg::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void Egg_OnTriggerEnter2D_m4555C712740903A6BE63018AF70B4DD929C3C8A4 (void);
+// 0x00000007 System.Void Egg::.ctor()
+extern void Egg__ctor_mCDD6641D2E4929FF620CC110DE8DFD6AE6994125 (void);
+// 0x00000008 System.Void GameManager::Awake()
+extern void GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F (void);
+// 0x00000009 System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x0000000A System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x0000000B System.Void GameManager::SpawnEgg()
+extern void GameManager_SpawnEgg_mB3E633FFAC8CDD6CEE9B7A9FC91CC7E68435E873 (void);
+// 0x0000000C System.Collections.IEnumerator GameManager::SpawnEggs()
+extern void GameManager_SpawnEggs_m6E82E545BA4577EE481B2EA8E494B95152946D2D (void);
+// 0x0000000D System.Void GameManager::AddScore()
+extern void GameManager_AddScore_mE906C522AAECEC56C7FEC7DD92A0D6C85CB9EFF0 (void);
+// 0x0000000E System.Void GameManager::GameStart()
+extern void GameManager_GameStart_m13D7CF637F648EE27334A03FF9B2132DA21A643B (void);
+// 0x0000000F System.Void GameManager::GameOver()
+extern void GameManager_GameOver_mF1BD400E7F84A0B533A58E80ADA7CCB89C964625 (void);
+// 0x00000010 System.Void GameManager::Replay()
+extern void GameManager_Replay_m94D590E6E95A33BB48EB1D7B7D46EFF4B1911EBB (void);
+// 0x00000011 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000012 System.Void GameManager/<SpawnEggs>d__16::.ctor(System.Int32)
+extern void U3CSpawnEggsU3Ed__16__ctor_m4ABEDCBE625366FE07A6A8E10A4ACAE38ADAE194 (void);
+// 0x00000013 System.Void GameManager/<SpawnEggs>d__16::System.IDisposable.Dispose()
+extern void U3CSpawnEggsU3Ed__16_System_IDisposable_Dispose_mDECDA154C7070C0822DF022C9710CD7A10D72E3D (void);
+// 0x00000014 System.Boolean GameManager/<SpawnEggs>d__16::MoveNext()
+extern void U3CSpawnEggsU3Ed__16_MoveNext_m30F93C22B479FCAAD206EB22D21A15649CF181DD (void);
+// 0x00000015 System.Object GameManager/<SpawnEggs>d__16::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSpawnEggsU3Ed__16_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m2B05C91A19A8727EACBFB273BF2C95B16CF6ED13 (void);
+// 0x00000016 System.Void GameManager/<SpawnEggs>d__16::System.Collections.IEnumerator.Reset()
+extern void U3CSpawnEggsU3Ed__16_System_Collections_IEnumerator_Reset_m316A83FAC0447FCA32E71B6D9A0556671E7B3F21 (void);
+// 0x00000017 System.Object GameManager/<SpawnEggs>d__16::System.Collections.IEnumerator.get_Current()
+extern void U3CSpawnEggsU3Ed__16_System_Collections_IEnumerator_get_Current_mEB3F7C8F0589AA677DA3118805A8000C1FEE6055 (void);
+static Il2CppMethodPointer s_methodPointers[23] = 
+{
+	BasketController_Start_mC696348DB8DD127709ED40F9D590DF298DD8A8A2,
+	BasketController_Update_m9C2E66B77A67B820A46A47AF6CAE2B1988A121B0,
+	BasketController__ctor_m261695708574EA9EAA5F70C368F7E5F8966035B2,
+	Egg_Start_m2A6F76BB43BFB3A4F6E9272801C4102838A419AC,
+	Egg_Update_m0B8158A925C8E1789B6E3890C76E375D86A62026,
+	Egg_OnTriggerEnter2D_m4555C712740903A6BE63018AF70B4DD929C3C8A4,
+	Egg__ctor_mCDD6641D2E4929FF620CC110DE8DFD6AE6994125,
+	GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_SpawnEgg_mB3E633FFAC8CDD6CEE9B7A9FC91CC7E68435E873,
+	GameManager_SpawnEggs_m6E82E545BA4577EE481B2EA8E494B95152946D2D,
+	GameManager_AddScore_mE906C522AAECEC56C7FEC7DD92A0D6C85CB9EFF0,
+	GameManager_GameStart_m13D7CF637F648EE27334A03FF9B2132DA21A643B,
+	GameManager_GameOver_mF1BD400E7F84A0B533A58E80ADA7CCB89C964625,
+	GameManager_Replay_m94D590E6E95A33BB48EB1D7B7D46EFF4B1911EBB,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	U3CSpawnEggsU3Ed__16__ctor_m4ABEDCBE625366FE07A6A8E10A4ACAE38ADAE194,
+	U3CSpawnEggsU3Ed__16_System_IDisposable_Dispose_mDECDA154C7070C0822DF022C9710CD7A10D72E3D,
+	U3CSpawnEggsU3Ed__16_MoveNext_m30F93C22B479FCAAD206EB22D21A15649CF181DD,
+	U3CSpawnEggsU3Ed__16_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m2B05C91A19A8727EACBFB273BF2C95B16CF6ED13,
+	U3CSpawnEggsU3Ed__16_System_Collections_IEnumerator_Reset_m316A83FAC0447FCA32E71B6D9A0556671E7B3F21,
+	U3CSpawnEggsU3Ed__16_System_Collections_IEnumerator_get_Current_mEB3F7C8F0589AA677DA3118805A8000C1FEE6055,
+};
+static const int32_t s_InvokerIndices[23] = 
+{
+	3444,
+	3444,
+	3444,
+	3444,
+	3444,
+	2807,
+	3444,
+	3444,
+	3444,
+	3444,
+	3444,
+	3352,
+	3444,
+	3444,
+	3444,
+	3444,
+	3444,
+	2790,
+	3444,
+	3293,
+	3352,
+	3444,
+	3352,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	23,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
